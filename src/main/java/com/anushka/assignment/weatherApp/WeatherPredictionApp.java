b4291ad60a315daa261948ac3d520b1a16481c44
Anushka25032000 <you@example.com>
2024-01-29 04:28:40 +0530

    ui

--- src/main/java/com/anushka/assignment/weatherApp/WeatherPredictionApp.java
+++ src/main/java/com/anushka/assignment/weatherApp/WeatherPredictionApp.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
-//@ComponentScan(basePackages = {"com.anushka.assignment.weatherApp.service", "com.anushka.assignment.weatherApp.controller"})
 @EnableCaching
 public class WeatherPredictionApp {
 
